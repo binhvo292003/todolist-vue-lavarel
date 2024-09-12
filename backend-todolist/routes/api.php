53b8ce10e20b597dd3676bfd03e9b87b5640304a
Binh VO <95414345+binhvo292003@users.noreply.github.com>
2024-09-12 17:36:55 +0700

    init auth with laravel breeze

--- backend-todolist/routes/api.php
+++ backend-todolist/routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Http\Controllers\TodoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TodoController;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+});
 
 Route::get('todos', [TodoController::class, 'index']);
 
